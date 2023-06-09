dfc6ab7dcf7347db06bf6f19e32923ec6fa36c40
Andrew Creekmore <andrew.creekmore@me.com>
2023-06-09 13:33:26 -0500

    updated default paddle positions; restricted player movement to Y-axis

--- Pong/Game.cpp
+++ Pong/Game.cpp
@@ -3,19 +3,17 @@
 
 static void simulateGame(Input* input, float deltaTime)
 {
-	clearScreen(0xff5500);
+	clearScreen(0x000000);
 
-	if (isDown(KEY_UP)) { playerPosition_y += playerSpeed * deltaTime; }
-	if (isDown(KEY_DOWN)) { playerPosition_y -= playerSpeed * deltaTime; }
-	if (isDown(KEY_LEFT)) { playerPosition_x -= playerSpeed * deltaTime; }
-	if (isDown(KEY_RIGHT)) { playerPosition_x += playerSpeed * deltaTime; }
+	// handle player (right paddle) movement input
+	if (isDown(KEY_UP) || isDown(KEY_W)) { playerPosition_y += playerSpeed * deltaTime; }
+	if (isDown(KEY_DOWN) || isDown(KEY_S)) { playerPosition_y -= playerSpeed * deltaTime; }
 
-	if (isDown(KEY_W)) { playerPosition_y += playerSpeed * deltaTime; }
-	if (isDown(KEY_S)) { playerPosition_y -= playerSpeed * deltaTime; }
-	if (isDown(KEY_A)) { playerPosition_x -= playerSpeed * deltaTime; }
-	if (isDown(KEY_D)) { playerPosition_x += playerSpeed * deltaTime; }
+	// ball
+	drawRect(0, 0, 1, 1, 0x00ff22);
+	// left paddle
+	drawRect(-60, 30, 2, 15, 0xffff22);
+	// right paddle
+	drawRect(playerPosition_x, playerPosition_y, 2, 15, 0xffff22);
 
-	drawRect(playerPosition_x, playerPosition_y, 1, 1, 0x00ff22);
-	drawRect(30, 30, 5, 5, 0xffff22);
-	drawRect(-20, 20, 8, 3, 0xffff22);
 }
--- Pong/Game.h
+++ Pong/Game.h
@@ -4,6 +4,6 @@
 #define pressed(key) (input->keyState[key].bIsDown && input->keyState[key].bHasChanged)
 #define released(key) (!input->keyState[key].bIsDown && input->keyState[key].bHasChanged)
 
-float playerPosition_x = 0.f;
-float playerPosition_y = 0.f;
+float playerPosition_x = 60.f;
+float playerPosition_y = 30.f;
 float playerSpeed = 50.f; // units per second
